e119fbc196de64ba851ee03d2ad9984900f3982a
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-08-04 13:51:37 -0500

    use mdl lite

--- src/main/elm/View.elm
+++ src/main/elm/View.elm
@@ -1,6 +1,5 @@
 module View(view)  where
 import Html exposing (..)
-import Component.PaperDrawerPanel exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Model exposing (Organization,Model)
@@ -14,8 +13,11 @@ view output =
      Ok model  -> renderModel model
 
 renderModel model =
-       drawerPanel (text "meow") (text "meow")
+       div [][mdlStylesheet,mdlIcons,mdlScript,xbutton]
      -- node "paper-drawer-panel" [][ orgsView model.orgs, text (toString "Meow")]
+xbutton = 
+  button [class "mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"] [ i [class "material-icons"][text "add"]]
+
 
 orgsView: List Organization -> Html
 orgsView orgs =
@@ -30,3 +32,37 @@ orgView org =
      ] 
    [(text org.name)]
    ]
+
+mdlStylesheet =
+    let
+        tag = "link"
+        attrs =
+            [ attribute "rel"       "stylesheet"
+            , attribute "property"  "stylesheet"
+            , attribute "href"      "//storage.googleapis.com/code.getmdl.io/1.0.2/material.indigo-pink.min.css"
+            ]
+        children = []
+    in 
+        node tag attrs children   
+
+mdlIcons =
+    let
+        tag = "link"
+        attrs =
+            [ attribute "rel"       "stylesheet"
+            , attribute "property"  "stylesheet"
+            , attribute "href"      "//fonts.googleapis.com/icon?family=Material+Icons"
+            ]
+        children = []
+    in 
+        node tag attrs children   
+mdlScript =
+    let
+        tag = "script"
+        attrs =
+            [
+             attribute "src"      "//storage.googleapis.com/code.getmdl.io/1.0.2/material.min.js"
+            ]
+        children = []
+    in 
+        node tag attrs children   
